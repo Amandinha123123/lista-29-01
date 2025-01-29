5caee0079c2fac1ce2aa2a933bf9552e6c7e0c2c
AMANDA DE SOUSA SANTOS <amanda.ssantos151@senacsp.edu.br>
2025-01-29 17:46:47 -0300

    atividade

---
+++ exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 25;
+            const double PI = 31.14159;
+
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("Valor de PI: " + PI);
+
+            Console.ReadLine();
+        }
+    }
+}
